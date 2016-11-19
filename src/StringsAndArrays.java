cc46554a3e04b60786eb6cb4c28756b5de9403d1
Piyush Ravi <mails4ipiyush@gmail.com>
2016-11-20 02:21:26 +0530

    add URLify()

--- src/StringsAndArrays.java
+++ src/StringsAndArrays.java
@@ -17,7 +17,9 @@ public class StringsAndArrays {
 	public static void main(String[] args) {
 		//System.out.println(isUnique("qwertyasdfghj"));
 		//System.out.println(isPermutation1("system", "sysmex"));
-		System.out.println(isPermutation2("system", "sysmet"));
+		//System.out.println(isPermutation2("system", "sysmet"));
+		char [] a = "Mr John Smith                     ".toCharArray();
+		URLify(a,"||||", 13);
 	}
 	
 	public static boolean isPermutation1(String a, String b) {
@@ -54,6 +56,38 @@ public class StringsAndArrays {
 		
 		return true;
 		
+	}
+	
+	public static void URLify(char [] a, String token, int length) {
+		
+		for(int i=0; i<length; i++) {
+			if(a[i] == ' ') {
+				shift(a, i, token.length()-1, length);
+				push(a, i, token);
+				i = i+token.length();
+				length += token.length()-1;
+			}
+		}
+		
+		for(int i = 0; i<length; i++) {
+			System.out.print(a[i]);
+		}
+	}
+
+	private static void push(char[] a, int start, String token) {
+		for(int i = 0; i < token.length(); i++) {
+			a[start++] = token.charAt(i);
+		}
+		
+		//System.out.println(Arrays.toString(a));
+	}
+
+	private static void shift(char[] a, int start, int shift, int length) {
+		int end = length - 1;
+		for(int i=end; i>start; i--) {
+			a[i+shift] = a[i];
+		}
+		//System.out.println(Arrays.toString(a));
 	}
 
 }
